023b46635fce1812294f7d0d0935bd8476ba93cc
DELL <DELL@DESKTOP-D3BQM73>
2023-05-09 21:43:59 -0700

    Add project files.

---
+++ stuApi/Models/AppDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+
+namespace MartialArtApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+
+
+        }
+        public DbSet<Sports> Sports { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //product Table
+            modelBuilder.Entity<Sports>().HasData(
+                new Sports
+                {
+                    ProductId = 1,
+                    ProductName = "Gloves",
+                    Email = "Azhar@786.com",
+                    State = "Punjab",
+                    Address = "Daska",
+                    Phone = "03311427734",
+
+                });
+            modelBuilder.Entity<Sports>().HasData(
+               new Sports
+               {
+                   ProductId = 2,
+                   ProductName = "Bat",
+                   Email = "az@786.com",
+                   State = "Punjab",
+                   Address = "Daska",
+                   Phone = "03311227734",
+
+               });
+            modelBuilder.Entity<Sports>().HasData(
+               new Sports
+               {
+                   ProductId = 3,
+                   ProductName = "SportsKit",
+                   Email = "Ans@786.com",
+                   State = "Punjab",
+                   Address = "Daska",
+                   Phone = "03313427734",
+
+               });
+            modelBuilder.Entity<Sports>().HasData(
+               new Sports
+               {
+                   ProductId = 4,
+                   ProductName = "showes",
+                   Email = "anr@786.com",
+                   State = "Punjab",
+                   Address = "Daska",
+                   Phone = "03321427734",
+
+               });
+        }
+    }
+}
---
+++ stuApi/Sports.cs
@@ -0,0 +1,18 @@
+﻿namespace MartialArtApi
+{
+    public class Sports
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+        public string Address { get; set; }
+        
+
+        
+        public string State { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        
+    }
+}
